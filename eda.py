c8a40a57fed3b91926ed3a7eed0f73369411c2cb
jkworldchampion <jkworldchampion@ajou.ac.kr>
2021-05-01 15:01:35 +0000

    eda 내의 파일 불러오는 위치 수정

--- eda.py
+++ eda.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import io
+import requests
 
-energy = pd.read_csv("./contest/energy.csv")
-energy.head()
+energy = pd.read_csv("https://raw.githubusercontent.com/cjfghk5697/forecastsolar/main/csv%20file/energy.csv") 
 
-plt.hist(energy)
+print(energy.head(3), energy.tail(3)) #18년 3월, 19년, 20년, 21년 1월
+
